950d0480a812d6f5ee2b64795f5a33748c2dcb4a
s-ly <sergeylysov@outlook.com>
2024-10-26 18:36:57 +0300

    Добавьте файлы проекта.

---
+++ Assets/Objects/Player/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float mouseSensitivity = 100f; // чувствительность выши
+    float speedPlayer = 15.0f; // скорость игрока
+    float xRotation = 0f;
+    Rigidbody rigPlayer;
+    Transform transPlayer;
+    Transform transCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InItPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control();
+    }
+
+    private void FixedUpdate()
+    {
+        ControlFixed();
+    }
+
+    void ControlFixed()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigPlayer.AddForce(transform.forward * speedPlayer);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigPlayer.AddForce(transform.forward * -speedPlayer);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigPlayer.AddForce(transform.right * -speedPlayer);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigPlayer.AddForce(-transform.right * -speedPlayer);
+        }
+    }
+
+    void Control()
+    {
+        // ѕолучаем ввод мыши
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; // ѕоворот камеры по оси X (вверх-вниз)
+        // ќграничиваем поворот камеры, чтобы она не могла повернутьс€ слишком далеко
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // ѕоворот игрока по оси Y (влево-вправо)
+        transPlayer.Rotate(Vector3.up * mouseX);
+
+        transCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // поворот камеры
+    }
+
+    void InItPlayer()
+    {
+        rigPlayer = GetComponent<Rigidbody>();
+        transPlayer = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked; // —крываем и блокируем курсор мыши
+        transCamera = transform.GetChild(0);
+    }
+}
